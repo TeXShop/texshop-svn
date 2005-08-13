6aaa97bdd96cf03414043abde46f03f312e30f07
Max Horn <max@quendi.de>
2005-08-13 18:53:48 +0000

    Updated GPL header to the new FSF address
    git-svn-id: https://svn.code.sf.net/p/texshop/code/trunk@54 8c80bfd2-15fc-0310-8b7a-ba317743b8bb

--- Sources/NSString-Extras.h
+++ Sources/NSString-Extras.h
@@ -1,10 +1,24 @@
-//
-//  NSString-Extras.h
-//  TeXShop
-//
-//  Created by Max Horn on 08.08.05.
-//  Copyright 2005 __MyCompanyName__. All rights reserved.
-//
+/*
+ * TeXShop - TeX editor for Mac OS 
+ * Copyright (C) 2000-2005 Richard Koch
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ *
+ */
 
 #import <Foundation/Foundation.h>
 
--- Sources/NSString-Extras.m
+++ Sources/NSString-Extras.m
@@ -1,21 +1,22 @@
-/* TeXShop - TeX editor for Mac OS 
+/*
+ * TeXShop - TeX editor for Mac OS 
  * Copyright (C) 2000-2005 Richard Koch
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
-
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
-
+ * 
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * $Id: TSDocument.m 32 2005-08-07 00:22:58Z maxhorn $
+ * $Id$
  *
  */
 
